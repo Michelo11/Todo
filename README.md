d0ac80fd008e5b1c4b353b6b1fee39de8ddff1d8
Michele <hello@michelemanna.me>
2024-08-31 12:54:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Todo
+
+Simple and easy todo fullstack app.
+
+## Installation
+
+```bash
+pnpm install
+
+# Start the development server
+pnpm dev
+
+# Build the project
+pnpm build
+
+# Start the production server
+pnpm start
+```
---
+++ api/src/index.ts
@@ -0,0 +1,59 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import prisma from "./lib/prisma";
+
+const app = new Hono();
+
+app.get("/", async (ctx) => {
+  const todos = await prisma.todo.findMany();
+
+  return ctx.json(todos);
+});
+
+app.post("/new", async (ctx) => {
+  const body = await ctx.req.json();
+
+  const todo = await prisma.todo.create({
+    data: {
+      title: body.title,
+    },
+  });
+
+  return ctx.json(todo);
+});
+
+app.patch("/update/:id", async (ctx) => {
+  const id = ctx.req.param("id");
+  const body = await ctx.req.json();
+
+  const todo = await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      completed: body.completed,
+    },
+  });
+
+  return ctx.json(todo);
+});
+
+app.delete("/delete/:id", async (ctx) => {
+  const id = ctx.req.param("id");
+
+  const todo = await prisma.todo.delete({
+    where: {
+      id,
+    },
+  });
+
+  return ctx.json(todo);
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
