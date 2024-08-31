ed1684e8ebd0a83b0ddd5753cf33807574c17e18
Michele <hello@michelemanna.me>
2024-08-31 17:48:39 +0200

    Finished app

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ pnpm build
 # Start the production server
 pnpm start
 ```
+
+https://github.com/user-attachments/assets/88b37bcb-404c-4fd1-8712-8e59ff988097
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,67 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from "./components/ui/card";
+import { Input } from "./components/ui/input";
+import React from "react";
+import { Checkbox } from "./components/ui/checkbox";
+import { Button } from "./components/ui/button";
 
 function App() {
+  const queryClient = useQueryClient();
+
   const { data: todos } = useQuery({
     queryKey: ["todos"],
     queryFn: () => fetch("http://localhost:3000/").then((res) => res.json()),
   });
+
+  const addTodo = useMutation({
+    mutationFn: (title: string) => {
+      return fetch("http://localhost:3000/new", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title }),
+      }).then((res) => res.json());
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  const completeTodo = useMutation({
+    mutationFn: ({ id, completed }: { id: string; completed: boolean }) => {
+      return fetch(`http://localhost:3000/update/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ completed }),
+      }).then((res) => res.json());
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  const deleteTodo = useMutation({
+    mutationFn: (id: string) => {
+      return fetch(`http://localhost:3000/delete/${id}`, {
+        method: "DELETE",
+      }).then((res) => res.json());
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  const [title, setTitle] = React.useState("");
 
   if (!todos) {
     return <p className="p-6">Loading...</p>;
@@ -27,15 +77,50 @@ function App() {
         <CardContent>
           <ul>
             {todos.map((todo: any) => (
-              <li key={todo.id}>
-                <input type="checkbox" checked={todo.completed} />
-                {todo.title}
+              <li key={todo.id} className="flex justify-between items-center">
+                <div className="flex items-center gap-3 ">
+                  <Checkbox
+                    id="todo"
+                    checked={todo.completed}
+                    onCheckedChange={(checked) => {
+                      completeTodo.mutate({
+                        id: todo.id,
+                        completed: !!checked,
+                      });
+                    }}
+                  />
+                  {todo.title}
+                </div>
+
+                <Button
+                  variant="destructive"
+                  onClick={() => {
+                    deleteTodo.mutate(todo.id);
+                  }}
+                >
+                  Delete
+                </Button>
               </li>
             ))}
           </ul>
 
           {todos.length === 0 && <p>No todos found.</p>}
         </CardContent>
+        <CardFooter>
+          <form
+            className="w-full"
+            onSubmit={(e) => {
+              e.preventDefault();
+              addTodo.mutate(title);
+            }}
+          >
+            <Input
+              placeholder="Add a new todo..."
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </form>
+        </CardFooter>
       </Card>
     </main>
   );
